97d1a66f62179b636902cee11ff38419464df293
jazzman80 <77989227+jazzman80@users.noreply.github.com>
2022-02-24 20:03:40 +0300

    Life System Continue

--- Assets/Managers/Scripts/LifeRegenerator.cs
+++ Assets/Managers/Scripts/LifeRegenerator.cs
@@ -1,18 +1,22 @@
 using UnityEngine;
 using UnityEngine.UI;
 using System;
+using TMPro;
 
 // Regenerates life after certain system time
 
 public class LifeRegenerator : MonoBehaviour
 {
     [SerializeField] private AppVariables appVariables;
+    [SerializeField] private EventBus eventBus;
     [SerializeField] private Timer timer;
     [SerializeField] private Button startLevelButton;
+    [SerializeField] private TextMeshProUGUI buttonText;
 
     private void Start()
     {
         timer.completedEvent.AddListener(OnTimerComplete);
+        timer.tickEvent.AddListener(OnTimerTick);
 
         while (DateTime.Compare(DateTime.Now, appVariables.nextLifeTime) > 0 && appVariables.Life < 3)
         {
@@ -27,7 +31,11 @@ public class LifeRegenerator : MonoBehaviour
     {
         int timeSeconds = (int)(appVariables.nextLifeTime - DateTime.Now).TotalSeconds;
         timer.Play(timeSeconds);
-        if (appVariables.Life > 0) startLevelButton.interactable = true;
+        if (appVariables.Life > 0)
+        {
+            startLevelButton.interactable = true;
+            buttonText.text = "START";
+        }
     }
 
     private void OnTimerComplete()
@@ -35,6 +43,17 @@ public class LifeRegenerator : MonoBehaviour
         appVariables.Life++;
         appVariables.nextLifeTime = DateTime.Now.AddMinutes(appVariables.LifeRegenerationTime);
         if (appVariables.Life < 3) StartNewLifeTimer();
+    }
+
+    private void OnTimerTick(int remainingTime)
+    {
+        if (appVariables.Life == 0)
+        {
+            int minutes = remainingTime / 60;
+            int seconds = remainingTime % 60;
+
+            buttonText.text = minutes.ToString() + " : " + seconds.ToString();
+        }
     }
 
 }
